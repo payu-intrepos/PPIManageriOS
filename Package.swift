1232a56b1a0dc27427c8e02d75dd9f5a40e698ce
Shubham Garg <91garg.shubham@gmail.com>
2026-03-09 22:04:00 +0530

    update alpha

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(name: "PayUIndia-CrashReporter",url: "https://github.com/payu-intrepos/PayUCrashReporter-iOS",from: "4.0.2"),
-        .package(name: "PayUIndia-Analytics",url: "https://github.com/payu-intrepos/PayUAnalytics-iOS",from: "4.1.0.alpha.1"),
+        .package(name: "PayUIndia-Analytics",url: "https://github.com/payu-intrepos/PayUAnalytics-iOS",branch: "alpha"),
         .package(name: "PayUIndia-NetworkReachability", url: "https://github.com/payu-intrepos/PayUNetworkReachability-iOS", from: "2.1.1")
     ],
     targets: [
